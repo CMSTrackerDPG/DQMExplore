2412999c1ae056710d16db4de286f0603564cbae
roy-cruz <roy.cruz@upr.edu>
2025-06-10 18:56:12 +0200

    Update dials API authentication

--- src/dqmexplore/utils/setupdials.py
+++ src/dqmexplore/utils/setupdials.py
@@ -1,9 +1,6 @@
 def setup_dials_object_deviceauth():
     from cmsdials import Dials
-    from cmsdials.auth.client import AuthClient
     from cmsdials.auth.bearer import Credentials
 
-    auth = AuthClient()
-    token = auth.device_auth_flow()
-    creds = Credentials.from_authclient_token(token)
+    creds = Credentials.from_creds_file()
     return Dials(creds)
